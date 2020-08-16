28193ea658b45b2384046720918ca5eaa4095632
Saquib89 <sakinshabd@gmail.com>
2020-08-17 02:14:36 +0600

    done

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const api = {
 
 const updateUI = data => {
     document.getElementById('show_city').innerText = data.name || "Unknown Location!";
-    document.getElementById('show_temparature').innerText = data.main.temp;
+    document.getElementById('show_temperature').innerText = data.main.temp;
     document.getElementById('weather_status').innerText = data.weather[0].main;
     document.getElementById('icon').setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
     document.getElementById('city').value = "";
